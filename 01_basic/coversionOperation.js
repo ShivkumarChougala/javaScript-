fb3b671c590e43ba409c26ad8b46c23d116d7b56
ShivkumarChougala <147145925+ShivkumarChougala@users.noreply.github.com>
2024-03-08 16:43:37 +0000

    Conversion

---
+++ 01_basic/coversionOperation.js
@@ -0,0 +1,31 @@
+let age = undefined
+
+// console.log(typeof age);
+
+let input = Number(age)
+
+//console.log(typeof input);
+//console.log(input);
+
+// 33 => number
+ // "33ab" => NaN not an number 
+ // null => 0
+ // undefined => NaN
+
+
+ let login = 0
+
+ let booleanlogin = Boolean(login)
+
+ //console.log(booleanlogin);
+
+ // yes it gave me true
+
+ // 1 => true; 0 => false
+ // " " => false; "Daddy" => true
+
+ let name = "ssss"
+
+ let stringName = String(name)
+
+ console.log(stringName);
