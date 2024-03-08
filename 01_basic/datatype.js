0973efb0d37987d785f084d9d2453a7e7d8a8436
ShivkumarChougala <147145925+ShivkumarChougala@users.noreply.github.com>
2024-03-08 16:20:44 +0000

    datatypes

---
+++ 01_basic/datatype.js
@@ -0,0 +1,18 @@
+
+
+let name = "Daddy"
+let age = "21"
+
+
+// number 
+// string => " " 
+// booleran => true/false
+// null => empty 
+// let cups = null
+
+// undefined =>  
+// let city; its show undefined 
+
+
+console.log(typeof null); // object
+
