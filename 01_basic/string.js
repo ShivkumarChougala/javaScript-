fe8cd8ab3464a3e7295ebe7e5230e61311891731
ShivkumarChougala <147145925+ShivkumarChougala@users.noreply.github.com>
2024-03-09 12:55:46 +0000

    string

---
+++ 01_basic/string.js
@@ -0,0 +1,22 @@
+let name = "Daddy"
+let Age = 21
+
+// console.log(`My Name is ${name} nd my Age is ${Age}`); My Name is Daddy nd my Age is 21
+
+// console.log(name.length); => 5
+// console.log(name.toUpperCase()); => DADDY
+// console.log(name[3]); => d its 3 letter in my name
+
+
+//  const newString = name.substring(0,4) => Dadd
+
+
+// const anotherName = name.slice(-5,1)
+// In slice we come from last digits also
+
+// console.log(anotherName)
+
+// const newName = "    Daddy   "
+// console.log(newName);
+// console.log(newName.trim()); 
+// Its remove extra space
