0d242bdfca2e536598ab9c1939f36680682e41d7
ShivkumarChougala <147145925+ShivkumarChougala@users.noreply.github.com>
2024-03-09 13:16:44 +0000

    number

---
+++ 01_basic/number_math.js
@@ -0,0 +1,11 @@
+let score = 400
+// console.log(score); => 400
+
+ let newScore = new Number(200) // => [Number: 200]
+console.log(newScore.toString()); // now its coverted in string you can string prototype like lenght and all 
+
+// console.log(newScore.toString().length); => 3
+
+let anotherNumber = 23.66857
+console.log(anotherNumber.toFixed(3));  // =>23.669  
+console.log(anotherNumber.toPrecision(4)); // => 23.67
