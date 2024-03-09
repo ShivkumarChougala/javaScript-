510404fe0efd2a80886cff2c0a5caee2cbe865ce
ShivkumarChougala <147145925+ShivkumarChougala@users.noreply.github.com>
2024-03-09 10:06:34 +0000

    operation

---
+++ 01_basic/operations.js
@@ -0,0 +1,38 @@
+// operation //
+
+let value = 9
+let negValue = -value
+// console.log(negValue); => -9
+
+
+// console.log(2+2);
+// - / * 
+// console.log(3%3);  Usefully
+
+
+let str1 = "Hello"
+let str2 = " World"
+let str3 = str1 + str2
+// console.log(str3); Hello World
+
+
+// console.log(2 + 2 + "3"); => 43 
+// if string at last the first 2 number will added 
+
+// console.log("2" + 2 + 3); => 223
+// if string is at first every thing declare as string
+
+
+
+// prefix and postfix
+
+// let x = 3
+// const y = x++
+
+ // console.log(`x:${x},y:${y}`); output: x:4, y:3
+
+
+ let x = 5
+const y = ++x
+
+// console.log(`x:${x},y:${y}`) output : x:6,y:6
