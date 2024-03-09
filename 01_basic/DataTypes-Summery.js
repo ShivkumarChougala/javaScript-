cd8110beca235ebc5aed613b6b5103cda08d31c7
ShivkumarChougala <147145925+ShivkumarChougala@users.noreply.github.com>
2024-03-09 10:45:40 +0000

    ty

---
+++ 01_basic/DataTypes-Summery.js
@@ -0,0 +1,33 @@
+// Primitive
+
+// string,number,boolern,null,undefined,sumbol,bigInt
+
+
+// let name = "Daddy" => string
+// const value = 100 => number
+// let login = false => boolern
+// let temp = null => null
+// let city; => undefined
+
+// Symbol
+
+let id = Symbol('22')
+
+
+// console.log(id);
+
+
+// Reference (nonPrimitive)
+
+// Array,Object,Function
+
+ // const hero = ["Shaktiman", "SuperMan"] => Array
+
+//  let myObj = {
+//     name = "Daddy",
+//     age = 21,
+//  }   // AnyThing Written In { is An Object}
+
+ const myFunction = function(){
+    console.log("hello World");
+ }
---
+++ 01_basic/comparison.js
@@ -0,0 +1,17 @@
+// Basic comparsion
+// console.log(2 = 1);
+// console.log(2 != 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+
+// string n Datatype
+// console.log("2" > 1);
+// console.log(2 < "1");
+
+// equality === is different  from comparison
+// console.log(null == 0); false
+// console.log(null == 0); false
+// console.log(null <= 0); true
+
+
+
