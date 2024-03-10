1939304120fedc7e264b3a769f724f65e91fe5ef
ShivkumarChougala <147145925+ShivkumarChougala@users.noreply.github.com>
2024-03-10 20:36:31 +0000

    Array

---
+++ 02_basic/array.js
@@ -0,0 +1,35 @@
+/////// Array //////
+
+const hero = ["shatiman","spiderman","superman"]
+
+// console.log(hero);
+
+
+
+ /// arrayMethod
+ const arry = [1,2,3,4,5]
+ arry.push(10) // it will number 
+ arry.pop()  // it will remove Last number
+//  console.log(arry);
+
+
+ const newArr = arry.join()  // this convert array into string
+
+//  console.log(arry);  // [ 1, 2, 3, 4, 5 ]
+//  console.log(newArr); // 1,2,3,4,5
+
+//  console.log(newArr.includes(3)); // true
+
+
+ // slice nd splice
+
+
+ const mySliceArr = [1,2,3,4,5,6]
+ console.log("A",mySliceArr);
+ console.log(mySliceArr.slice(1,3)); // [ 2, 3 ]
+ 
+
+ const mySpliceArr = [1,2,3,4,5,6]
+ console.log("B",mySliceArr);
+ console.log(mySliceArr.splice(1,3));
+
