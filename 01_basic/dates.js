83f533123221a99f2d1ae873dc282584642daf1d
ShivkumarChougala <147145925+ShivkumarChougala@users.noreply.github.com>
2024-03-10 06:02:53 +0000

    Date

---
+++ 01_basic/dates.js
@@ -0,0 +1,7 @@
+let myDate = new Date()
+console.log(myDate.toDateString()); // Sun Mar 10 2024
+
+console.log(typeof myDate); // object
+
+let myCurrentDate = new Date(2024,0,10) //here prefer to Jan
+console.log(myCurrentDate.toDateString()); // Wed Jan 10 2024  
